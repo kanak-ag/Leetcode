efe596fa507a72de2841e4dbcb85b9a662ec8579
Kanak Agrawal <133563970+kanak-ag@users.noreply.github.com>
2024-05-09 17:20:55 +0530

    Time: 3 ms (5.53%), Space: 42.3 MB (49.26%) - LeetHub

---
+++ 1431-kids-with-the-greatest-number-of-candies/1431-kids-with-the-greatest-number-of-candies.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max=0;
+        for(int i=0;i<candies.length;i++){
+            if (candies[i]>max){
+                max=candies[i];
+            }
+        }
+        System.out.println(max);
+        List<Boolean> ans = new ArrayList<>();
+        for(int i=0;i<candies.length;i++){
+            if(candies[i]+extraCandies>=max){
+                // max=candies[i]+extraCandies;
+                ans.add(true);
+            }
+            else{
+                ans.add(false);
+            }
+        }
+        return ans;
+        
+    }
+}
