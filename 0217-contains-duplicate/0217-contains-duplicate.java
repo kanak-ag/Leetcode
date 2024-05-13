166177eed1fe469eeebf26e4c7d33af1e3a9aba0
Kanak Agrawal <133563970+kanak-ag@users.noreply.github.com>
2024-05-13 14:20:03 +0530

    Time: 13 ms (41.65%), Space: 58.9 MB (39.04%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        
+        HashMap<Integer,Integer> hm=new HashMap<Integer,Integer>();
+        for (int i=0;i<nums.length;i++){
+            if(hm.containsKey(nums[i])){
+                return true;
+            }
+            hm.put(nums[i],i);
+        } 
+        return false;
+    }
+}
